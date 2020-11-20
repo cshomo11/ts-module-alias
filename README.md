c2555048ce6a7fdf33feb0f878d53ab3d00fd3cf
Luke Caswell Samuel <luke.caswell.samuel@gmail.com>
2020-11-20 16:24:28 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,147 @@
+This package is a major refactor of [ilearnio/module-alias](https://github.com/ilearnio/module-alias) using TypeScript.  This package changes some behaviors of the original.  Namely:
+
+1. More recently added paths take priority over existing paths during module resolution.
+2. Only the resolution paths of the immediate parent module are modified.
+
+----
+
+# ts-module-alias
+
+Create aliases of directories and register custom module paths in Node.
+
+It also allows you to register aliases and directories that will act just like `node_modules` but with your own private modules, so that you can access them directly:
+
+## Install
+
+```
+npm i --save @lukecsamuel/ts-module-alias
+```
+
+## Usage
+
+Add your custom configuration to your `package.json` (in your application's root)
+
+```json
+// Aliases
+"_moduleAliases": {
+  "@root"      : ".", // Application's root
+  "@deep"      : "src/some/very/deep/directory/or/file",
+  "@my_module" : "lib/some-file.js",
+  "something"  : "src/foo", // Or without @. Actually, it could be any string
+}
+
+// Custom module directories, just like `node_modules` but with your private modules (optional)
+"_moduleDirectories": ["node_modules_custom"],
+```
+
+Initialize the new resolution rules within a module by creating an instance and passing the current module:
+
+```ts
+import ModuleAlias from '@lukecsamuel/ts-module-alias';
+new ModuleAlias();
+```
+
+## Advanced usage
+
+If you don't want to modify your `package.json` or you just prefer to set it all up programmatically, then the following methods are available for you:
+
+* `addAlias('alias', 'target_path')` - register a single alias
+* `addAliases({ 'alias': 'target_path', ... }) ` - register multiple aliases
+* `addPath(path)` - Register custom modules directory (like node_modules, but with your own modules)
+
+_Examples:_
+```ts
+import ModuleAlias from '@lukecsamuel/ts-module-alias';
+const moduleAlias = new ModuleAlias(module);
+
+//
+// Register alias
+//
+moduleAlias.addAlias('@client', '/src/client');
+
+// Or multiple aliases
+moduleAlias.addAliases({
+  '@root'  : '.',
+  '@client': '/src/client',
+  ...
+});
+
+// Custom handler function (starting from v2.1)
+moduleAlias.addAlias('@src', (fromPath, request, alias) => {
+  // fromPath - Full path of the file from which `require` was called
+  // request - The path (first argument) that was passed into `require`
+  // alias - The same alias that was passed as first argument to `addAlias` (`@src` in this case)
+
+  // Return any custom target path for the `@src` alias depending on arguments
+  if (fromPath.startsWith('/others')) {
+    return '/others';
+  }
+  return '/src';
+});
+
+//
+// Register custom modules directory
+//
+moduleAlias.addPath('/node_modules_custom');
+moduleAlias.addPath('/src');
+```
+
+## Usage with WebPack
+
+Luckily, WebPack has a built in support for aliases and custom modules directories so it's easy to make it work on the client side as well!
+
+```js
+// webpack.config.js
+const npm_package = require('./package.json');
+
+module.exports = {
+  entry: { ... },
+  resolve: {
+    root: __dirname,
+    alias: npm_package._moduleAliases || {},
+    modules: npm_package._moduleDirectories || [] // eg: ["node_modules", "node_modules_custom", "src"]
+  }
+};
+```
+
+More details on the [official documentation](https://webpack.js.org/configuration/resolve).
+
+## Usage with Jest
+
+Unfortunately, `module-alias` itself would not work from Jest due to a custom behavior of Jest's `require`. But you can use it's own aliasing mechanism instead. The configuration can be defined either in `package.json` or `jest.config.js`. The example below is for `package.json`:
+
+```json
+"jest": {
+  "moduleNameMapper": {
+    "@root/(.*)": "<rootDir>/$1",
+    "@client/(.*)": "<rootDir>/src/client/$1"
+  },
+}
+```
+
+More details on the [official documentation](https://jestjs.io/docs/en/configuration#modulenamemapper-objectstring-string--arraystring).
+
+## Using within another NPM package
+
+You can use `module-alias` within another NPM package, however there are a few things to take into consideration.
+
+1. As the aliases are global, you should make sure your aliases are unique, to avoid conflicts with end-user code, or with other libraries using module-alias. For example, you could prefix your aliases with '@my-lib/', and then use require('@my-lib/deep').
+
+## Known incompatibilities
+
+This module does not play well with:
+
+- Front-end JavaScript code. Module-alias is designed for server side so do not expect it to work with front-end frameworks (React, Vue, ...) as they tend to use Webpack. Use Webpack's [resolve.alias](https://webpack.js.org/configuration/resolve/#resolvealias) mechanism instead.
+- [Jest](https://jestjs.io), which discards node's module system entirely to use it's own module system, bypassing module-alias.
+- The [NCC compiler](https://github.com/zeit/ncc), as it uses WebPack under the hood without exposing properties, such as resolve.alias. It is not [something they wish to do](https://github.com/zeit/ncc/pull/460).
+
+## How it works?
+
+In order to register an alias it modifies the internal `Module._resolveFilename` method so that when you use `require` or `import` it first checks whether the given string starts with one of the registered aliases, if so, it replaces the alias in the string with the target path of the alias.
+
+In order to register a custom modules path (`addPath`) it modifies the internal `Module._nodeModulePaths` method so that the given directory then acts like it's the `node_modules` directory.
+
+[npm-image]: https://img.shields.io/npm/v/module-alias.svg
+[npm-url]: https://npmjs.org/package/module-alias
+[travis-image]: https://img.shields.io/travis/ilearnio/module-alias/master.svg
+[travis-url]: https://travis-ci.org/ilearnio/module-alias
---
+++ index.ts
@@ -0,0 +1,286 @@
+import Path from 'path';
+
+type FuncNodeModulePaths = (from: string) => string[]
+type FuncResolveFileName = (request: string, parent: NodeModule, ...args: unknown[]) => string
+
+interface ModuleAliasDict {
+  [key: string]: ((from: string, request: string, alias: string) => string) | string
+}
+
+export type NodeModuleConstructor =
+  (new (...args: any[]) => NodeModule)
+  & {
+    _cache: { [key: string]: NodeModule }
+    _contextLoad: boolean
+    _debug: unknown
+    _extensions: { [key: string]: string }
+    _findPath: (request: string, paths: string[]) => string | false
+    _load: (request: string, parent: NodeModule, isMain: boolean) => unknown
+    _nodeModulePaths: FuncNodeModulePaths
+    _pathCache: { [key: string]: string }
+    _realPathCache: unknown
+    _resolveFilename: FuncResolveFileName
+    _resolveLookupPaths: (request: string, parent: NodeModule) => [string, string[]]
+
+    _moduleAlias?: ModuleAlias
+
+    globalPaths: string[]
+    wrapper: unknown
+    wrap: unknown
+  }
+
+export interface ModuleAliasOptions {
+  base?: string
+}
+
+/**
+ * Implements the module-alias library, which allows programmatic configuration of module resolution rules,
+ * as a transient class which applies to the calling module.
+ */
+export class ModuleAlias {
+  /**
+   * The module used by the module resolver.
+   */
+  private module: NodeModule
+  /**
+   * The module class used by the module resolver.
+   */
+  private ModuleCtor: NodeModuleConstructor
+  /**
+   * Captured instance of the old _nodeModulePaths function.
+   */
+  private oldNodeModulePaths: FuncNodeModulePaths
+  /**
+   * Captured instance of the old _resolveFilename function
+   */
+  private oldResolveFilename: FuncResolveFileName
+
+
+  /**
+   * A list of paths for which modules are searched.
+   */
+  private modulePaths: string[] = [];
+  /**
+   * A dictionary of aliases and their expanded path representations.
+   */
+  private moduleAliases: ModuleAliasDict = {};
+  /**
+   * A list of aliases.
+   */
+  private moduleAliasNames: string[] = [];
+
+
+  constructor (parent?: NodeModule, givenOptions?: ModuleAliasOptions | string) {
+    // Guard against poorly mocked module constructors
+    if (parent) {
+      this.module = parent;
+      this.ModuleCtor = parent.constructor as NodeModuleConstructor;
+    } else if (module.parent) {
+      this.module = module.parent;
+      this.ModuleCtor = module.parent.constructor as NodeModuleConstructor;
+    } else {
+      throw new Error('Failed to initialize ts-module-alias.  `parent` was not defined and a suitable substitute could not be found.');
+    }
+
+    // Guard against initializing on the same module
+    if (this.ModuleCtor._moduleAlias) {
+      return this.ModuleCtor._moduleAlias;
+    }
+
+    this.oldNodeModulePaths = this.ModuleCtor._nodeModulePaths;
+    this.oldResolveFilename = this.ModuleCtor._resolveFilename;
+
+    const self = this;
+    this.ModuleCtor._nodeModulePaths = (from) => self.nodeModulePaths(self, from);
+    this.ModuleCtor._resolveFilename = (request, parent, ...args) => self.resolveFilename(self, request, parent, ...args);
+
+    let options: ModuleAliasOptions;
+    if (typeof givenOptions === 'string') {
+      options = { base: givenOptions };
+    } else {
+      options = givenOptions || {};
+    }
+
+    let candidatePackagePaths;
+    if (options.base) {
+      candidatePackagePaths = [Path.resolve(options.base.replace(/\/package\.json$/, ''))]
+    } else {
+      // There is probably 99% chance that the project root directory in located
+      // above the node_modules directory,
+      // Or that package.json is in the node process' current working directory (when
+      // running a package manager script, e.g. `yarn start` / `npm run start`)
+      candidatePackagePaths = [Path.join(__dirname, '../..'), process.cwd()];
+    }
+
+    let npmPackage;
+    let base;
+    for (let i in candidatePackagePaths) {
+      try {
+        base = candidatePackagePaths[i];
+        npmPackage = require(Path.join(base, 'package.json'));
+        break;
+      } catch (e) {
+        // noop
+      }
+    }
+
+    if (typeof npmPackage !== 'object') {
+      let pathString = candidatePackagePaths.join(',\n');
+      throw new Error(`Unable to find package.json in any of:\n[${pathString}]`);
+    }
+
+    //
+    // Import aliases
+    //
+
+    let aliases = {
+      ...npmPackage._moduleAliases
+    }
+
+    for (let alias in aliases) {
+      if (Path.isAbsolute(aliases[alias])) {
+        aliases[alias] = Path.join(base, aliases[alias]);
+      }
+    }
+
+    this.addAliases(aliases);
+
+    //
+    // Register custom module directories (like node_modules)
+    //
+
+    if (npmPackage._moduleDirectories instanceof Array) {
+      npmPackage._moduleDirectories.forEach((dir: string) => {
+        if (dir === 'node_modules') return;
+
+        let modulePath = Path.join(base, dir);
+        this.addPath(modulePath);
+      });
+    }
+
+    this.ModuleCtor._moduleAlias = this;
+  }
+
+  /**
+   * Adds a path to the source directories to search for modules
+   *
+   * @param path The path which should be used as a source directory
+   */
+  addPath (path: string): void {
+    path = Path.normalize(path);
+
+    if (this.modulePaths.indexOf(path) === -1) {
+      this.modulePaths.unshift(path);
+      this.addPathHelper(path, this.module.paths);
+    }
+  }
+
+  /**
+   * Adds an alias to a path
+   *
+   * @param alias Path alias
+   * @param target Actual path
+   */
+  addAlias (alias: string, target: string) {
+    this.moduleAliases[alias] = target;
+    // Cost of sorting is lower here than during resolution
+    this.moduleAliasNames = Object.keys(this.moduleAliases);
+    this.moduleAliasNames.sort();
+  }
+
+  /**
+   * Adds many aliases to paths
+   *
+   * @param aliases An object containing paths keyed by their alias.
+   */
+  addAliases (aliases: { [key: string]: string }) {
+    for (let alias in aliases) {
+      this.addAlias(alias, aliases[alias]);
+    }
+  }
+
+  /**
+   * Checks if the import request matches to an alias
+   * @param path Import request
+   * @param alias A path alias
+   */
+  isPathMatchesAlias (path: string, alias: string): boolean {
+      // Matching /^alias(\/|$)/
+      return path.indexOf(alias) === 0
+        && (path.length === alias.length || path[alias.length] === '/');
+  }
+
+  /**
+   * Normalizes a path and adds it to an array if it is not already present
+   * NOTE: feels redundant, idk, might delete later
+   *
+   * @param path The path to be added to an array
+   * @param targetArray The array to which the path should be added
+   */
+  private addPathHelper (path: string, targetArray: string[]) {
+    path = Path.normalize(path);
+    if (targetArray && targetArray.indexOf(path) === -1) {
+      targetArray.unshift(path);
+    }
+  }
+
+  /**
+   * Overloads the _nodeModulePaths function on the module system
+   *
+   * @param this unknown
+   * @param self Instance of current ModuleAlias class
+   * @param from The path of the requesting module
+   */
+  private nodeModulePaths(this: unknown, self: this, from: string): string[] {
+    let paths = self.oldNodeModulePaths.call(this, from);
+
+    // Only include the module path for top-level modules
+    // that were not installed:
+    if (from.indexOf('node_modules') === -1) {
+      paths = self.modulePaths.concat(paths);
+    }
+
+    return paths;
+  }
+
+  /**
+   * Overloads the _resolveFilename function on the module system
+   *
+   * @param this unknown
+   * @param self Instance of current ModuleAlias class
+   * @param request Import request string e.g. 'path', 'fs' or 'src/path/to/file'
+   * @param parent The module making this request
+   * @param args Further arguments, irrelevant to what this is doing
+   */
+  private resolveFilename(this: unknown, self: this, request: string, parent: NodeModule, ...args: unknown[]): string {
+    for (let i = self.moduleAliasNames.length; i-- > 0;) {
+      let alias = self.moduleAliasNames[i];
+      if (self.isPathMatchesAlias(request, alias)) {
+        let aliasTarget = self.moduleAliases[alias];
+        // Custom function handler
+        if (typeof self.moduleAliases[alias] === 'function') {
+          let fromPath = parent.filename;
+          aliasTarget = (self.moduleAliases[alias] as any)(fromPath, request, alias);
+          if (!aliasTarget || typeof aliasTarget !== 'string') {
+            throw new Error('[module-alias] Expecting custom handler function to return path.');
+          }
+        }
+        request = Path.join(aliasTarget as string, request.substr(alias.length));
+        // Only use the first match
+        break;
+      }
+    }
+
+    // Modify parent before passing to guarantee that module paths are included.
+    // This is kind of a hack to fix an issue with parent paths not populating correctly after hot-module reload.
+    //   Cause of actual bug is unknown, but this makes things work.
+    const paths = {};
+    for (let path of [...self.modulePaths, ...parent.paths]) {
+      paths[path] = true;
+    }
+    parent.paths = Object.keys(paths);
+
+    return self.oldResolveFilename.call(this, request, parent, ...args);
+  }
+}
+export default ModuleAlias;
